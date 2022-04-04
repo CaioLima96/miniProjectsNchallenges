84f627c3ec33e7de9d309006aff5756af6dc7b80
CaioLima96 <62268811+CaioLima96@users.noreply.github.com>
2022-04-04 19:31:07 -0300

    Feito o active dos numeros do Interactive Rating Comp

---
+++ interactive-rating-component/resources/js/main.js
@@ -0,0 +1,46 @@
+//=========== ACTIVE NUMBER BTN
+
+let numBtn = document.querySelectorAll('.numberBtn')
+console.log(numBtn)
+
+numBtn.forEach(button => {
+    button.addEventListener('click', function () {
+        numBtn.forEach(btn => btn.classList.remove('active'))
+        this.classList.add('active')
+    })
+})
+
+
+
+
+//=========== 
+
+
+
+document.getElementById('submitBtn').addEventListener('click', (event) => {
+    console.log('Alo')
+
+    let hideContent = document.getElementById('content').style.display = 'none'
+
+    let showThankYouMessage =  document.getElementById('thankYouMessage').innerHTML = `
+			
+        <div id="phoneIcon">
+            <img src="./assets/img/illustration-thank-you.svg" alt="Phone Icon">
+        </div>
+
+        <div id="ratingMessage">
+            <p>You selected 4 out of 5</p>
+        </div>
+
+        <div id="thankYouText">
+
+            <p>Thank you!</p>
+
+            <p>We appreciate you taking the time to give a rating. If you ever need more support, don’t hesitate to get in touch!
+            </p>
+        </div>
+
+    `
+
+    return showThankYouMessage
+})
