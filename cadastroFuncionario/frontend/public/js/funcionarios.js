0a2ff2b65573976349c5bb26a52aa941bb24d5f5
CaioLima96 <caiovcastrolima@gmail.com>
2023-10-24 18:25:35 -0300

    chore: agora a height do sideMain é definida automaticamente

--- cadastroFuncionario/frontend/public/js/funcionarios.js
+++ cadastroFuncionario/frontend/public/js/funcionarios.js
@@ -1,11 +1,25 @@
 console.log('funcionarios page')
 
+let tableTr = document.getElementsByClassName('tableContainer')[0].getElementsByTagName('tbody')[0].children
+let sidemain = document.getElementsByClassName('sideMain')[0]
 
-async function aaa() {
+
+if (tableTr.length < 8) {
+    sidemain.classList.add('sideMainHeight')
+    console.log('tr: ', tableTr.length)
+} else {
+    sidemain.classList.remove('sideMainHeight')
+    console.log('tr: ', tableTr.length)
+}
+
+
+
+
+async function api() {
 
     let data = await fetch(`http://localhost:3000/funcionarios`).then(res => res.json())
 
     console.log(data)
 }
 
-aaa()
+api()
