57803f2f32e0a66662891d827bdd03e724940a8f
CaioLima96 <caiovcastrolima@gmail.com>
2023-10-30 15:39:38 -0300

    fix: agora a table e sidebar não tem mais problema com a altura

--- cadastroFuncionario/frontend/public/js/funcionarios.js
+++ cadastroFuncionario/frontend/public/js/funcionarios.js
@@ -1,18 +1,5 @@
 console.log('funcionarios page')
 
-let tableTr = document.getElementsByClassName('tableContainer')[0].getElementsByTagName('tbody')[0].children
-let sidemain = document.getElementsByClassName('sideMain')[0]
-
-
-if (tableTr.length < 11) {
-    sidemain.classList.add('sideMainHeight')
-    console.log('tr: ', tableTr.length)
-} else {
-    sidemain.classList.remove('sideMainHeight')
-    console.log('tr: ', tableTr.length)
-}
-
-
 
 
 // async function api() {
